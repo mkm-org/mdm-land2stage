4d7f6a0f336de495aa4b43659942b70f3170eb34
test <test@me.org>
2022-06-10 19:52:06 -0400

    init

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-CONFIG_FILE_URL=/var/conf/apps/l2s/l2s_DEV.properties
+SET CONFIG_FILE_URI=/var/conf/apps/l2s/l2s_DEV.properties
 java -jar target\l2s-0.0.1-SNAPSHOT.jar
--- src/test/java/l2s/AppTest.java
+++ src/test/java/l2s/AppTest.java
@@ -1,8 +1,9 @@
 package l2s;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -13,8 +14,7 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue(){
         assertTrue( true );
     }
 }
